2e8dc63ec98fee26707a0697ae4dd963d1fde802
FelineJTD <16520335@std.stei.itb.ac.id>
2022-06-18 23:09:13 +0700

    add file.read to read file contents

--- src/main.rb
+++ src/main.rb
@@ -5,24 +5,25 @@ path = '../test/DropsuiteTest/**/*'
 num_of_same_content = {}
 
 Dir.glob(path).each do |file|
-  next if File.directory?(file) # skip the loop if the file is a directory
-  # puts file
+  next if File.directory?(file) # skip the loop if file is a directory
 
-  same = false
+  same = false # set same to false
   num_of_same_content.each_key do |key|
-    if FileUtils.cmp(file, key)
-      num_of_same_content[key] += 1
-      same = true
+    if FileUtils.cmp(file, key) # same file is found
+      num_of_same_content[key] += 1 # add number of occurences
+      same = true # set same to true
       break
     end
   end
 
   if !same
-    num_of_same_content[file] = 1
+    num_of_same_content[file] = 1 # register the file to the hash for next checks
   end
 end
 
-num_of_same_content.values.sort
-file, occurences = num_of_same_content.first
-puts file
-puts occurences
+num_of_same_content = num_of_same_content.sort_by { |k, v| v } # sort hash by values (number of occurences)
+file, occurences = num_of_same_content.last # get the last file (the most common)
+
+file = File.open(file) # read file content
+puts file.read, occurences
+file.close # close file
